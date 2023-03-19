a5d613d26121588693efaf3ef56b4b4ddcf78c56
Putra Prassiesa Abimanyu <ppabimanyu@gmail.com>
2023-03-19 21:09:39 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, request, jsonify
+import joblib
+import string
+import nltk
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+app = Flask(__name__)
+
+# load model
+model = joblib.load('classifier_v14_data3000_split80-20_ngram1-3_modeFeatureSeletionOptimizeAlphaPSO.joblib')
+# load vectorizer
+vectorizer = joblib.load('vectorizer_v14_data3000_split80-20_ngram1-3_modeFeatureSeletionOptimizeAlphaPSO.joblib')
+# load index_feature_selected
+index_feature_selected = joblib.load('ifs_v14_data3000_split80-20_ngram1-3_modeFeatureSeletionOptimizeAlphaPSO.joblib')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        text = request.json['text']
+        cleaned_text = preprocess_text(text)
+        transformed_text = vectorizer.transform([cleaned_text])
+        print(transformed_text.shape)
+        feature_selected = transformed_text[:, index_feature_selected]
+        print(feature_selected.shape)
+
+        result = model.predict(feature_selected)
+
+        prob = model.predict_proba(feature_selected)[0]
+
+        prediction = ''
+        if result[0] == 0:
+            prediction = 'valid'
+        elif result[0] == 1:
+            prediction = 'hoaks'
+        else:
+            prediction = 'disinformasi'
+
+        return jsonify({
+            'statusCode': 200,
+            'message': 'success',
+            'prediction': prediction,
+            'details': {
+                    'valid': '{:.2f}%'.format(prob[0] * 100),
+                    'hoaks': '{:.2f}%'.format(prob[1] * 100),
+                    'disinformasi': '{:.2f}%'.format(prob[2] * 100),
+                }
+            })
+    else:
+        return jsonify({
+            'statusCode': 500,
+            'error': 'Internal Server Error',
+            })
+
+
+# preprocessing
+def preprocess_text(text):
+
+    # lowercase
+    text = text.lower()
+
+    # tokenizing
+    tokens = nltk.tokenize.word_tokenize(text)
+
+    # remove punctuation
+    tokens = [word for word in tokens if word not in string.punctuation]
+
+    # remove number
+    # tokens = [word for word in tokens if word.isalpha()]
+
+    # remove stopwords
+    stopword = nltk.corpus.stopwords.words('indonesian')
+    tokens = [word for word in tokens if word not in stopword]
+
+    # stemming
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    stemmed = []
+    for word in tokens:
+        c = word.replace('.', '').replace(',', '')
+        if c.isnumeric():
+            stemmed.append(word)
+        else:
+            stemmed.append(stemmer.stem(word))
+
+    # join tokens menjadi text kembali
+    text = ' '.join(tokens)
+    
+    return text
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
